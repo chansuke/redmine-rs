af5764572bf59c65c19fe10b382c25b1ed2b2ef1
chansuke <chansuke@georepublic.de>
2021-02-15 23:08:39 +0900

    Add minimum implementation of post issue

--- src/resources/custom_field.rs
+++ src/resources/custom_field.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct CustomField {
     pub id: i32,
     pub name: String,
